41b00d200f99a63110d80a2da0b0e9c80315c7bf
iflix0fox <ahmadalazzawi5080@gmail.com>
2025-12-09 14:55:17 +0200

    moved files around

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import styles from "./Card.module.css";
-import animalEmojis from "../assets/animalEmojis.json";
+import animalEmojis from "../animalEmojis.json";
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import Button from "@mui/material/Button";
